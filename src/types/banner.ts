76a66c7a2cbf09c89f2a7ee1cb062bfb3cc73a74
BoYeungBang <57360625+4thdraw@users.noreply.github.com>
2025-12-29 10:33:52 +0900

    안양그린컴퓨터학원 리액트 평가

---
+++ src/types/banner.ts
@@ -0,0 +1,30 @@
+export interface MainBannerItem {
+  imgurl: string;
+  title: string;
+  subtitle: string;
+}
+
+export interface MainBannerJson {
+  mainBanner: MainBannerItem[];
+}
+
+export interface MainProduct {
+  섹션명: string
+  상품목록: ProductItem[]
+}
+
+export interface ProductItem {
+  productid : number
+  브랜드?: string
+  상품명: string
+  판매가: number
+  정가: number
+  배송: string
+  이미지:string
+  노출: boolean
+  등록날짜 : string
+}
+
+export interface MainProductResponse {
+  mainProduct: MainProduct
+}
---
+++ tailwind.config.js
@@ -0,0 +1,50 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+        maxWidth: {
+          '1550': '1550px'
+        },       
+        colors :{
+          main : "#ac441e",
+          gray : "#707070",
+          black :"#0A0909"
+        },
+        fontFamily :{
+          kr : ['Noto Sans KR', 'sans-serif'],
+          en : ['Montserrat', 'sans-serif'],
+        },
+        fontSize :{
+          title : '30px',
+          navi : '24px',
+          subtitle : '18px',
+          base : '16px',
+          small : '14px',
+          xs : '12px'
+        },
+        fontWeight :{
+            100 : '100',
+            200 : '200',
+            300 : '300',         
+            400 : '400',
+            500 : '500',
+            600 : '600',
+            700 : '700',
+            900 : '900'
+        },
+        screens: {
+          xs: "375px",   // 모바일 소형
+          sm: "578px",   // 모바일 대형
+          md: "820px",   // 태블릿
+          lg: "1024px",
+          xl: "1550px",  // 데스크톱 대형
+        },
+    },
+  },
+  plugins: [],
+}
+
